673ca9968d6319a5d5cb011deadb5b0a497b922c
comsaziz <comsaziz@gmail.com>
2024-09-15 10:53:58 +0300

    add

--- main.js
+++ main.js
@@ -1,23 +1,22 @@
+
 function updateBox() {
     const box = document.getElementById('box');
-    const width = document.getElementById('width').value;
-    const height = document.getElementById('height').value;
-    const color = document.getElementById('color').value;
-    const borderRadius = document.getElementById('borderRadius').value;
+    const Width = document.getElementById('width').value;
+    const Height = document.getElementById('height').value;
+    const Color = document.getElementById('color').value;
+    const newBorderRadius = document.getElementById('borderRadius').value;
 
-
-    box.style.width = width ? `${width}%` : '30%';
-    box.style.height = height ? `${height}%` : '40%';
-    box.style.backgroundColor = color ? color : 'color';
-    box.style.borderRadius = borderRadius ? `${borderRadius}px` : '50px';
+    box.style.width = Width ? Width + '%' : '30%';
+    box.style.height = Height ? Height + '%' : '40%';
+    box.style.backgroundColor = Color || 'blue';
+    box.style.borderRadius = newBorderRadius ? newBorderRadius + 'px' : '10px';
 }
 
 function reset() {
     const box = document.getElementById('box');
-    
-
     box.style.width = '30%';
     box.style.height = '40%';
     box.style.backgroundColor = 'blue';
     box.style.borderRadius = '10px';
 }
+
